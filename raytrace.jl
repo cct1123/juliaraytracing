75b963fc1f1ae18416f38074dc3da58555846655
monmister <thmsouscarlet@gmail.com>
2025-02-21 10:44:32 -0600

    construct object for ray trace

--- raytrace.jl
+++ raytrace.jl
@@ -83,13 +83,20 @@ struct  Frame
     
 end
 
-struct Surface
+abstract type Surface end
+
+struct ImplicitSurface <: Surface 
     # define the surface structure
     frame:: Frame # the frame of the surface
-    shape:: Function # bound functions specifying the 3d shape
+    shape:: Function # bound functions specifying the 3d shape, f(x, y, z)=0
     bounds:: Vector{Tuple{Float64, Float64}}
     border:: Function # bound functions specifying the boundary line
 end
+
+function shape_plane(center::Vec3)
+    return (p) -> p[3] - center[3]
+end
+
 
 # example shape function: Define a spherical surface as the shape function
 function shape_sphere(center::Vec3, radius::Float64)
@@ -118,12 +125,30 @@ function shape_mexican_hat(center::Vec3, A::Float64, B::Float64, C::Float64)
 end
 
 
-function bound_circle(center::Vec2, radius::Float64)
-    return (p) -> (p - center)⋅(p - center) - radius^2  # Circle equation: ||p - center|| = radius
+function shape_coneflat(center::Vec3, height::Float64, radius::Float64, angle::Float64)
+    function result(p)
+        x, y, z = p .- center  # Shift coordinates relative to the center
+        radial_dist = sqrt(x^2 + y^2)
+        r_top = radius
+        r_bottom = radius + height * tan(angle)
+
+        if radial_dist <= radius
+            return z-height  # Outside the top
+        elseif radial_dist <= r_bottom
+            return  z-height+(radial_dist-radius)/tan(angle) # Conical region
+        else
+            return z  # Flat bottom outside cone
+        end
+    end
+    return (p) -> result(p)
 end
 
-function bound_none(center::Vec2, radius::Float64)
-    return (p) -> -1  # no bound line defined
+function border_circle(radius::Float64)
+    return (p) -> p⋅p - radius^2  # Circle equation: ||p - center|| = radius
+end
+
+function border_bounds()
+    return (p) -> -1  #  bound line for surface is defined by bounds
 end
 
 function surface_normal(p, surface::Surface)
@@ -172,7 +197,7 @@ function draw_surface(surface:: Surface, nx::Int=100, ny::Int=100, color="random
     return [surface_data]
 end
 
-function draw_snormals(surface:: Surface, num:: Int, color="random")
+function draw_snormals(surface:: Surface, num:: Int,  ray_length=0.5, arrow_scale=0.3)
     xarray = range(surface.bounds[1][1], stop=surface.bounds[1][2], length=nx)
     yarray = range(surface.bounds[2][1], stop=surface.bounds[2][2], length=ny)
 
@@ -190,13 +215,13 @@ function draw_snormals(surface:: Surface, num:: Int, color="random")
         xx, yy, zz = surface.frame.orientation * [xx, yy, zz] + surface.frame.origin
         push!(rays_snorm, Ray([xx, yy, zz], snorm))
     end
-    return draw_rays(rays_snorm)
+    return draw_rays(rays_snorm, ray_length=ray_length, arrow_scale=arrow_scale)
 end
 
 # Function to generate and plot rays with arrows
 function draw_rays(rays::Vector{Ray}; ray_length=0.5, arrow_scale=0.3)
     traces = GenericTrace[]
-
+    color = string("rgb(", round(255 * rand()), ",", round(255 * rand()), ",", round(255 *  rand()), ")")
     # Line traces for the rays
     for ray in rays
         start = ray.origin
@@ -204,7 +229,7 @@ function draw_rays(rays::Vector{Ray}; ray_length=0.5, arrow_scale=0.3)
         trace_rays = scatter3d(
             x=[start[1], stop[1]], y=[start[2], stop[2]], z=[start[3], stop[3]],
             mode="lines",
-            line=attr(width=3),
+            line=attr(color=color,width=3)
         )
         push!(traces, trace_rays)
     end
@@ -216,16 +241,18 @@ function draw_rays(rays::Vector{Ray}; ray_length=0.5, arrow_scale=0.3)
     trace_arrows = cone(
         x=origins[1, :], y=origins[2, :], z=origins[3, :],  # Positions
         u=directions[1, :], v=directions[2, :], w=directions[3, :],  # Directions
-        sizemode="absolute",
+        sizemode="raw",
         sizeref=arrow_scale,  # Controls arrow size
         anchor="tail",
         showscale=false,
+        colorscale=[(0, color), (1, color)],
         opacity=0.8
     )
     push!(traces, trace_arrows)
 
     return traces
 end
+# ==============================================================================================================
 # ==============================================================================================================
 
 
@@ -236,37 +263,78 @@ if abspath(@__FILE__) == abspath(PROGRAM_FILE)
     bound_radius = 4.5
     x_range = (-5.0, 5.0)  # X range
     y_range = (-5.0, 5.0)  # Y range
-    nx = 200 # number of points along x
-    ny = 200 # number of points along y
-    num_sn = 200 # number of surface normals
+    nx = 50 # number of points along x
+    ny = 50 # number of points along y
+    num_sn = 100 # number of surface normals
     identity_matrix = rotation_matrix([0.0, 0.0, 1.0], 0.0) 
     rotx_matrix = rotation_matrix([1.0, 0.0, 0.0], π/4.0) 
+    roty45_matrix = rotation_matrix([0.0, 1.0, 0.0], -π/4.0) 
+    flip_matrix = rotation_matrix([1.0, 0.0, 0.0], π*1.0) 
+    
     labframe = Frame([0.0,10.0,0.0], identity_matrix)
     objframe = Frame([10.0,0.0,0.0], rotx_matrix)
-    bounds = [(x_range[1], x_range[2]), (y_range[1], y_range[2])]
-
-    parasurface = Surface(
+    objframe2 = Frame([0.0,-5.0,0.0], roty45_matrix)
+    objframe3 = Frame([0.0,-5.0,0.0], flip_matrix)
+    bounds = [x_range, y_range]
+    
+    parasurface = ImplicitSurface(
         labframe, 
-        # objframe,
-        # shape_paraboloid([0.0, 0.0, 0.0], focal_length), 
-        shape_mexican_hat([0.0, 0.0, 0.0], 4.0, 1.0, 1.0),
+        shape_paraboloid([0.0, 0.0, 0.0], focal_length), 
         bounds,
-        bound_circle([0.0, 0.0], bound_radius))
-
-
-
-    hemisurface = Surface(
+        border_circle(bound_radius))
+    
+        
+    parasurface = ImplicitSurface(
+        labframe, 
+        shape_paraboloid([0.0, 0.0, 0.0], focal_length), 
+        bounds,
+        border_circle(bound_radius))
+        
+    hemisurface = ImplicitSurface(
         objframe, 
         # shape_paraboloid([0.0, 0.0, 0.0], focal_length), 
         shape_hemisphere([0.0, 0.0, 0.0], hem_radius), 
         bounds,
-        bound_circle([0.0, 0.0], bound_radius))
-
-    traces1_sf = draw_surface(parasurface, nx, ny)
-    traces1_sn = draw_snormals(parasurface, num_sn)
-    traces2_sf = draw_surface(hemisurface, nx, ny)
-    traces2_sn = draw_snormals(hemisurface, num_sn)
-
+        border_circle(bound_radius))
+    
+    coneflatsurface = ImplicitSurface(
+        objframe2,
+        shape_coneflat([0.0, 0.0, -5.0], 4.0, 2.0, π/180*30),
+        bounds,
+        border_bounds()
+        # border_circle([0.0, 0.0], bound_radius)
+        )
+    
+    flatsurfarce1 = ImplicitSurface(
+        objframe2, 
+        # objframe,
+        shape_plane([0.0, 0.0, 5.0]),
+        bounds,
+        border_bounds()
+        # border_circle([0.0, 0.0], bound_radius)
+        )
+    
+    mhatsurface = ImplicitSurface(
+        objframe3, 
+        shape_mexican_hat([0.0, 0.0, 10.0], 4.0, 1.0, 1.0),
+        bounds,
+        border_bounds())
+    
+    
+    plottraces = vcat(
+        draw_surface(parasurface, nx, ny),
+        draw_snormals(parasurface, num_sn),
+        draw_surface(hemisurface, nx, ny),
+        draw_snormals(hemisurface, num_sn),
+        draw_surface(coneflatsurface, nx, ny),
+        draw_snormals(coneflatsurface, num_sn),
+        draw_surface(flatsurfarce1, nx, ny),
+        draw_snormals(flatsurfarce1, num_sn),
+        draw_surface(mhatsurface, nx, ny),
+        draw_snormals(mhatsurface, num_sn)
+    )
+    
+    
     layout = Layout(
         title="Surface Plot",
         width=800,   # Set width in pixels
@@ -281,7 +349,8 @@ if abspath(@__FILE__) == abspath(PROGRAM_FILE)
         paper_bgcolor="rgba(0,0,0,0)",  # Transparent outer background
         showlegend=false
     )
+    
     # Display the plot
-    fig = plot(vcat(traces1_sf,traces1_sn, traces2_sf, traces2_sn) , layout)
+    fig = plot(plottraces , layout);
     display(fig)
 end
